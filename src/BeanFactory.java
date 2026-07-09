f829885eedc8ed50f1d18ac90bf2e68683f301d5
adouska <adouska@pricefox.gr>
2026-07-09 14:08:34 +0300

    remove comments

--- src/BeanFactory.java
+++ src/BeanFactory.java
@@ -3,14 +3,12 @@ import java.lang.reflect.Constructor;
 public enum BeanFactory {
     INSTANCE;
 
-    public <T> T getInstanceOf(Class<T> type, Object... args /*zero or more arguments*/) { // Given a class, it returns an object of that class
+    public <T> T getInstanceOf(Class<T> type, Object... args) {
         try {
             Constructor<?> constructor = type.getDeclaredConstructors()[0];
-            // This uses reflection. Reflection means Java code can inspect classes while the program is running.
-            // Asking the class what constructor it has and gets the first one (0).
-            constructor.setAccessible(true); // allows to call this constructor even if it is not public
+            constructor.setAccessible(true);
 
-            return type.cast(constructor.newInstance(args)); // object creation of whatever class was passed in. Calls the constructor and passes the arguments.
+            return type.cast(constructor.newInstance(args));
         } catch (Exception e) {
             throw new RuntimeException("Could not create instance of " + type.getName(), e);
         }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BeanFactory beanFactory = BeanFactory.INSTANCE;
 
-        Message message1 = beanFactory.getInstanceOf(Message.class, "Hello"); //type = Message.class args = ["Hello"]
-        //same as Message message = new Message("Hello");
+        Message message1 = beanFactory.getInstanceOf(Message.class, "Hello");
         System.out.println("Message 1 = " + message1);
 
         Message message2 = beanFactory.getInstanceOf(Message.class, "world");
