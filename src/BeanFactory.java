bd6e0088253584183a7afde3c357d92de7f27711
adouska <adouska@pricefox.gr>
2026-07-09 14:16:55 +0300

    edit beans factory

--- src/BeanFactory.java
+++ src/BeanFactory.java
@@ -1,16 +1,23 @@
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 
 public enum BeanFactory {
     INSTANCE;
 
-    public <T> T getInstanceOf(Class<T> type, Object... args) {
+    public <T> T getInstanceOf(Class<T> beanClass, Object... arguments) {
         try {
-            Constructor<?> constructor = type.getDeclaredConstructors()[0];
-            constructor.setAccessible(true);
+            // Creating the array of the parameters types
+            Class<?>[] argumentsClasses = Arrays.stream(arguments).map(Object::getClass).toArray(Class<?>[]::new);
+            // Locating the corresponding constructor
+            Constructor<T> beanConstructor = beanClass.getConstructor(argumentsClasses);
+            // creating the bean
+            T bean = beanConstructor.newInstance(arguments);
 
-            return type.cast(constructor.newInstance(args));
-        } catch (Exception e) {
-            throw new RuntimeException("Could not create instance of " + type.getName(), e);
+            return bean;
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
         }
     }
 }
